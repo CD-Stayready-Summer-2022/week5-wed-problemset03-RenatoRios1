6f04e3e7bcc4b2fc4c6cd89f9458db8013cac7c1
RenatoRios1 <renatoarios1@gmail.com>
2022-07-06 13:57:54 -0500

    TIME: 1:57 No nested for loops (not even a small one)

--- src/main/java/com/codedifferently/problems/Problem02.java
+++ src/main/java/com/codedifferently/problems/Problem02.java
@@ -4,6 +4,31 @@ import java.util.ArrayList;
 
 public class Problem02 {
     public static Integer cousinTag(ArrayList<String> cousins, Integer startIndex, String targetName){
-        return null;
+        int stepsLeft = 0;
+        for(int i = startIndex; stepsLeft < cousins.size(); i--){
+            System.out.print (cousins.get(i) + " ");
+            if(cousins.get(i).equals(targetName)) {
+                break;
+            }
+            if(i == 0){
+                i = cousins.size();
+            }
+            stepsLeft++;
+        }
+        System.out.println();
+
+        int stepsRight = 0;
+        for(int i = startIndex; stepsRight < cousins.size(); i++){
+            System.out.print (cousins.get(i) + " ");
+            if(cousins.get(i).equals(targetName)){
+                break;
+            }
+            if(i == cousins.size()-1){
+                i = -1;
+            }
+            stepsRight++;
+        }
+        System.out.println();
+        return Math.min(stepsLeft, stepsRight);
     }
 }
